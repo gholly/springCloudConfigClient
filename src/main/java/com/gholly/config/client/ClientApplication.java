7bb6443f49e9559c9bea314ef1ba1fe51e64c752
Guan Huifang <hfguan@thoughtworks.com>
2018-03-12 17:14:56 +0800

    inited commit

--- src/main/java/com/gholly/config/client/ClientApplication.java
+++ src/main/java/com/gholly/config/client/ClientApplication.java
@@ -4,17 +4,22 @@ package com.gholly.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 @SpringBootApplication
+@RefreshScope
+@EnableConfigurationProperties
 public class ClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ClientApplication.class, args);
 
     }
+
 
 
 
